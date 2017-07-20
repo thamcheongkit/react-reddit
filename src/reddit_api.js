b2c15de3beefe84820bf8cb1bfb1bc37e7e21a7e
ck <thamcheongkit@gmail.com>
2017-07-20 18:43:18 +0800

    change fetch to request

--- src/reddit_api.js
+++ src/reddit_api.js
@@ -1,10 +1,26 @@
+// export default class RedditAPI {
+//   static fetchRedditJson = (callback, url='', query='') => {
+//     const URL = (process.env.NODE_ENV === 'production') ? '/v1' + url + '.json' + query : url + '.json' + query;
+//     fetch(URL, {accept: 'application/json',})
+//       .then(resp=>resp.json())
+//       .then(data=>data.data.children)
+//       .then(lines=>lines.map(line=>line.data))
+//       .then(data=>callback(data))
+//   }
+// }
+
+var request = require('request');
+
 export default class RedditAPI {
   static fetchRedditJson = (callback, url='', query='') => {
-    const URL = (process.env.NODE_ENV === 'production') ? '/v1' + url + '.json' + query : url + '.json' + query;
-    fetch(URL, {accept: 'application/json',})
-      .then(resp=>resp.json())
-      .then(data=>data.data.children)
-      .then(lines=>lines.map(line=>line.data))
-      .then(data=>callback(data))
+    const URL = 'https://www.reddit.com' + url + '.json' + query;
+    console.log(URL);
+    request(URL, function (error, response, body) {
+      if (!error) {
+        const data = JSON.parse(body);
+        const lines = data.data.children;
+        callback(lines.map(line=>line.data));
+      }
+    });
   }
 }
