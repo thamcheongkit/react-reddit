5f84b55b920edec48cf5af12343011f7ca7f42cb
ck <thamcheongkit@gmail.com>
2017-12-07 09:13:27 +0800

    add github ribbon

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -4,17 +4,21 @@ import dropdown from './DropDownIcon.svg';
 import Transition from 'react-transition-group/Transition';
 import Comments from '../comments';
 import { Route } from 'react-router-dom'
+import GithubRibbon from "../GithubRibbon/GithubRibbon";
 
 const DEFAULT_SUBREDDITS = ["/r/announcements/","/r/Art/","/r/AskReddit/","/r/askscience/","/r/aww/","/r/blog/","/r/books/","/r/creepy/","/r/dataisbeautiful/","/r/DIY/","/r/Documentaries/","/r/EarthPorn/","/r/explainlikeimfive/","/r/food/","/r/funny/","/r/Futurology/","/r/gadgets/","/r/gaming/","/r/GetMotivated/","/r/gifs/","/r/history/","/r/IAmA/","/r/InternetIsBeautiful/","/r/Jokes/","/r/LifeProTips/","/r/listentothis/","/r/mildlyinteresting/","/r/movies/","/r/Music/","/r/news/","/r/nosleep/","/r/nottheonion/","/r/OldSchoolCool/","/r/personalfinance/","/r/philosophy/","/r/photoshopbattles/","/r/pics/","/r/science/","/r/Showerthoughts/","/r/space/","/r/sports/","/r/television/","/r/tifu/","/r/todayilearned/","/r/UpliftingNews/","/r/videos/","/r/worldnews/"]
 
 const UI = (props) => (
   <div>
-    <Header pathname={props.location.pathname} />
-    <Dropdown>
-      <Subreddits />
-    </Dropdown>
-    <Posts data={props.data} {...props} />
-    <NextPage {...props} />
+    <GithubRibbon />
+    <div className="react-reddit">
+      <Header pathname={props.location.pathname} />
+      <Dropdown>
+        <Subreddits />
+      </Dropdown>
+      <Posts data={props.data} {...props} />
+      <NextPage {...props} />
+    </div>
   </div>
 );
 
